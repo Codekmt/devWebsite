860b0d53de62cb4acc61787febf8391b5703383d
Keanu MTei <keanumt@gmail.com>
2025-10-01 17:51:59 +0200

    more navBar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 
 import { useEffect, useState } from 'react';
 
@@ -26,7 +31,7 @@ function NavBar() {
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={''} alt="Logo" />
+            <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
         <span className='navbar-toggler-icons'></span>
@@ -39,9 +44,9 @@ function NavBar() {
           </Nav>
           <span className='navbar-text'>
             <div className='social-icons'>
-                <a href='#'><img src={} alt=''/></a>
-                <a href='#'><img src={} alt=''/></a>
-                <a href='#'><img src={} alt=''/></a>
+                <a href='#'><img src={navIcon1} alt=''/></a>
+                <a href='#'><img src={navIcon2} alt=''/></a>
+                <a href='#'><img src={navIcon3} alt=''/></a>
             </div>
             <button className='vvd' onClick={() => console.log('connect')}><span>Let's Connect</span></button>
           </span>
