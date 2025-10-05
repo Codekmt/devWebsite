02f9a5998e3270bd79973e0b78637797e8390ed9
codeKMT <keanumt@gmail.com>
2025-10-05 20:42:27 +0200

    Fixed scroll-behavior

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -48,7 +48,7 @@ export const Banner = () => {
                     <Col xs={12} md={6} xl={7}>
                     <span className="tagLine">Welcome to my portfolio</span>
                     <h1>Hi I'm Keanu <br /><span className="wrap">{text}</span></h1>
-                    <p>Hello! After completing a full stack development course, I’m diving into my first project and loving the experience. I’m now eager to find my first job or internship to further sharpen my skills and contribute to meaningful projects.</p>
+                    <p>Hello! After completing a full stack development course, I’m diving into my first project and loving the experience. I’m now eager to find my first job or internship to further sharpen my skills and contribute to meaningful projects.<br /> I speak Dutch, French and English fluently so contact me how you prefer!</p>
                     <a href="#contact">
                         <button>
                         Let's connect <ArrowRightCircle size={25} />
