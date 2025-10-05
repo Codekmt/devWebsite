883cd7a1592fe7dd852f66bd28e2348e4c23814f
codeKMT <keanumt@gmail.com>
2025-10-05 21:32:16 +0200

    Add CI=false to script

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ import headerImg from '../assets/img/header-img.svg';
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Junior Web Developer", "Web Designer", "UX/UI Designer"];
+    const toRotate = ["Junior Web Developer", "Web Designer", "Fullstack Developer"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 -Math.random() * 100);
     const period = 2000;
