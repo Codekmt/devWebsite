c4f9f1a8b85ece5480ad8c83311c4c9777ab3a9d
codeKMT <keanumt@gmail.com>
2025-10-07 15:29:11 +0200

    Try different way to add link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
 
 export const Contact = () => {
-  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+  const API_URL = "https://personalportfolio-kgm7.onrender.com/api/contact";
   const [formDetails, setFormDetails] = useState({
     firstName: "",
     lastName: "",
